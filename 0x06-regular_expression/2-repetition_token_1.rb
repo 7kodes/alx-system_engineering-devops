4c202ecc8a1b0598275398cebfd2da6b85f31cc3
7kodes <200397421+7kodes@users.noreply.github.com>
2025-03-25 03:32:03 +0100

    Fix bug

--- 0x06-regular_expression/2-repetition_token_1.rb
+++ 0x06-regular_expression/2-repetition_token_1.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/env ruby
+# This regular expression matches:
+# htn
+# hbtn
 
 input = ARGV[0]
 
-matches = input.scan(/[^abc]/).join
+matches = input.scan(/hb?tn/).join
 
 print matches
-
-# RUN:
-# ./2-repetition_token_1.rb abcdefghi | cat -e
-# 
-# OUTPUTS:
-# defghi
